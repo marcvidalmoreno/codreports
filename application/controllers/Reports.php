78330d558e5af55a2a5585e7fbc6180759fb84d0
Marc Vidal Moreno <marc.vidalmoreno@alphanetworks.be>
2017-05-20 00:31:25 +0200

    Created controller and view with all reports list - Printing_r associative array

---
+++ application/controllers/Reports.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Reports extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/reports
+	 *	- or -
+	 * 		http://example.com/index.php/reports/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/reports/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$reports = $this->db->query('SELECT * FROM cod_report')->result();
+
+		$data['reports'] = $reports;
+
+		$this->load->view('reports', $data);
+	}
+}
