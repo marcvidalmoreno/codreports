126f61620571380ceed4e475a48c390f1983e9e1
Marc Vidal Moreno <marc.vidal.moreno@gmail.com>
2017-08-22 00:46:12 +0200

    Routing - Removed index.php from urls - Added htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://codreports.local/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
